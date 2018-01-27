5d2d59eb55fc5d6cfe05ff4728a061b66c43117a
Funsis <s.korovin95@gmail.com>
2018-01-27 16:53:24 +0300

    Create Footer component with style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Grid } from "react-bootstrap";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const App = () => {
   return (
     <Grid fluid={true}>
       <Header />
+      <Footer />
     </Grid>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Grid } from "react-bootstrap";
+import "./Fotter.css";
 
 const Footer = () => {
   return (
-    <Grid fluid={true}>
-      
+    <Grid fluid={true} className="footer">
+      <a href="https://github.com/funsis" className="footer__copyright">by Funsis</a>
     </Grid>
   );
 };
+
+export default Footer;
