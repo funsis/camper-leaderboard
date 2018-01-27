2f953ece7533f1c637eb631a7c4f23823471106c
Funsis <s.korovin95@gmail.com>
2018-01-27 19:52:09 +0300

    Create leaders reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import leadersReducer from "./leaders_reducer";
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  leaders: leadersReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/leaders_reducer.js
@@ -0,0 +1,13 @@
+import FETCH_RECENT from "../actions/types";
+import FETCH_ALLTIME from "../actions/types";
+
+export default function leadersReducer(state = [], action) {
+  switch(action.type) {
+    case FETCH_RECENT:
+      return [action.payload.data];
+    case: FETCH_ALLTIME:
+      return: [action.payload.data];
+    default:
+      return state;
+  }
+}
