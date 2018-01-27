8328f2e10c4925cdea8760ba975abd99983074bd
Funsis <s.korovin95@gmail.com>
2018-01-27 19:52:44 +0300

    Create leaders reducer

--- src/reducers/leaders_reducer.js
+++ src/reducers/leaders_reducer.js
@@ -5,8 +5,8 @@ export default function leadersReducer(state = [], action) {
   switch(action.type) {
     case FETCH_RECENT:
       return [action.payload.data];
-    case: FETCH_ALLTIME:
-      return: [action.payload.data];
+    case FETCH_ALLTIME:
+      return [action.payload.data];
     default:
       return state;
   }
